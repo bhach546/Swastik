f2cf2af8ac2459a52ae47aa536eccad905b9949d
bhavyasreechinthala <chinthala0327@gmail.com>
2025-04-12 06:35:34 -0500

    replace a project1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Layout from './Container/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        Hey Bharat
+      </header> */}
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
